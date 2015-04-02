ece23ab7e4605721bdb8427f7f85152333a39d94
EricLindemann <elindemann3@gmail.com>
2015-04-02 14:25:50 -0600

    added untracked

---
+++ Easel/app/src/main/java/cs/easel/DrawPath.java
@@ -0,0 +1,16 @@
+package cs.easel;
+
+/**
+ * Created by Eric on 4/1/15.
+ */
+public class DrawPath {
+    public int width;
+
+    public DrawPath(int startWidth){
+        width = startWidth;
+    }
+
+    public void setWidth(int newWidth){
+        width = newWidth;
+    }
+}
---
+++ Easel/app/src/unitTest/java/cs/easel/test/LauncherTest.java
@@ -0,0 +1,40 @@
+package cs.easel.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import cs.easel.Launcher;
+import cs.easel.R;
+
+/**
+ * Created by eric on 4/2/15.
+ */
+public class LauncherTest extends ActivityInstrumentationTestCase2<Launcher> {
+
+    Launcher activity;
+
+    public LauncherTest(){
+        super(Launcher.class);
+    }
+
+    protected void setUp() throws Exception {
+        super.setUp();
+        activity = getActivity();
+    }
+
+    public void testButtonNotNull(){
+        Button button = (Button) activity.findViewById(R.id.button);
+        assertNotNull(button);
+    }
+
+    public void testButton2NotNull(){
+        Button button2 = (Button) activity.findViewById(R.id.button);
+        assertNotNull(button2);
+    }
+
+    public void testImageViewNotNull(){
+       // ImageView imageView = (ImageView) activity.findViewById(R.id.imageView);
+        //assertNotNull(imageView);
+    }
+}
